66d709324f2013bc78e21cf0f7d48c369471e65b
Joshua Cadavez <jcadavez@insider.com>
2022-01-03 08:16:07 -0600

    Ch10.1 Death Animation State

--- Player.gd
+++ Player.gd
@@ -10,6 +10,7 @@ var gravity : int = 400
 
 var vel : Vector2 = Vector2()
 var grounded : bool = false
+var alive = true
 
 # components
 onready var sprite = $AnimatedSprite
@@ -21,43 +22,48 @@ func _ready():
 
 # Called 60 times a second to define Player's physics calculations
 func _physics_process(delta):
-	# reset the horizontal velocity
-	vel.x = 0
-	
-	# movement inputs 
-	if Input.is_action_pressed("move_left"):
-		vel.x -= speed 
-	if Input.is_action_pressed("move_right"):
-		vel.x += speed
+	# Player moves only if alive
+	if alive:
+		# reset the horizontal velocity
+		vel.x = 0
 		
-	# plays "float" animation if it's moving
-	if vel.x != 0:
-		sprite.play("float")
-	else:
-		sprite.stop()
+		# movement inputs 
+		if Input.is_action_pressed("move_left"):
+			vel.x -= speed 
+		if Input.is_action_pressed("move_right"):
+			vel.x += speed
+			
+		# plays "float" animation if it's moving
+		if vel.x != 0:
+			sprite.play("float")
+		else:
+			sprite.stop()
+			
+		# applying the velocity 
+		vel = move_and_slide(vel, Vector2.UP)
+		# iterate through all objects that slide against the player
+		for i in get_slide_count():
+			var collision = get_slide_collision(i)
+			if "Enemy" in collision.collider.name:
+				die()
 		
-	# applying the velocity 
-	vel = move_and_slide(vel, Vector2.UP)
-	# iterate through all objects that slide against the player
-	for i in get_slide_count():
-		var collision = get_slide_collision(i)
-		if "Enemy" in collision.collider.name:
-			die()
-	
-	# gravity
-	vel.y += gravity * delta
-	
-	# jump input
-	if Input.is_action_pressed("jump") and is_on_floor():
-		vel.y -= jumpForce
+		# gravity
+		vel.y += gravity * delta
 		
-	# sprite direction
-	if vel.x > 0:
-		sprite.flip_h = true 
-	elif vel.x < 0:
-		sprite.flip_h = false
+		# jump input
+		if Input.is_action_pressed("jump") and is_on_floor():
+			vel.y -= jumpForce
+			
+		# sprite direction
+		if vel.x > 0:
+			sprite.flip_h = true 
+		elif vel.x < 0:
+			sprite.flip_h = false
 		
 func die():
+	alive = false
+	sprite.play("death")
+	yield(get_tree().create_timer(2), "timeout")
 	get_tree().reload_current_scene()
 	
 # called when we run into a coin
